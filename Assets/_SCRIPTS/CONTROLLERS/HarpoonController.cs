7e82fab91eb9822c7a22b6b7f04250b114b6d971
Romain D <36967424+Rhuminoux@users.noreply.github.com>
2023-08-26 17:28:35 +0200

    Harpoon, collectible fish, menu

--- Assets/_SCRIPTS/CONTROLLERS/HarpoonController.cs
+++ Assets/_SCRIPTS/CONTROLLERS/HarpoonController.cs
@@ -1,18 +1,53 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HarpoonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 10;
+    public Transform rifleOrigin;
 
-    // Update is called once per frame
+    private Rigidbody2D m_rigidbody2D;
+    private CollectibleFishController m_collectedFish;
+    private bool isRetracting;
+    private Vector3 m_direction;
+
     void Update()
     {
-        
+        if (!isRetracting)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+            if (Vector2.Distance(transform.position, rifleOrigin.position) > 8)
+            {
+                isRetracting = true;
+            }
+        }
+        else
+        {
+            m_direction = (rifleOrigin.position - transform.position).normalized;
+            transform.position += m_direction * speed * Time.deltaTime;
+            if (Vector2.Distance(transform.position, rifleOrigin.position) < 0.2f)
+            {
+                rifleOrigin.GetComponent<HarpoonRifleController>().ReloadHarpoon(m_collectedFish);
+                Destroy(gameObject);
+            }
+        }    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (1 << collision.gameObject.layer == LayerMask.GetMask("CollectibleFish"))
+        {
+            isRetracting = true;
+            GetComponent<Collider2D>().enabled = false;
+            speed = 4;
+            if (collision.TryGetComponent<CollectibleFishController>(out m_collectedFish))
+            {
+                transform.position = m_collectedFish.transform.position;
+                GetComponent<SpriteRenderer>().enabled = false;
+                m_collectedFish.GetCaught(transform);
+            }
+        }
     }
 }
